a3aa34cd3ec0903f03545cc2e9f274cd4859dbd0
jberthiaume1138 <joenyc23@gmail.com>
2017-04-08 03:24:02 -0400

    add social media icons to fotter

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router';
 
 import styles from './style/Footer.scss';
 
+import twitter from '../../images/twitter.svg';
+import linkedin from '../../images/linkedin.png';
+
 export default class Footer extends React.Component {
 	render () {
 		return (
@@ -13,7 +16,11 @@ export default class Footer extends React.Component {
 						<Link to="services" activeClassName={styles.activeStyle} >Services</Link>
 						<Link to="contact" activeClassName={styles.activeStyle}>Contact</Link>
 					</div>
-					<p className={styles.footerText}>Downtown New York since 1991</p>
+					<div className={styles.social}>
+						<a href="https://twitter.com/carlfriedberg" target="_blank"><img src={twitter} height='35' /></a>
+						<a href="https://www.linkedin.com/in/carlfriedberg/" target="_blank"><img src={linkedin} height='25' /></a>
+					</div>
+					<div className={styles.footerText}>Downtown New York since 1991</div>
 				</div>
 			</footer>
 		);
