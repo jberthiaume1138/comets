53ec6e1232de89209a530c44a2dc28ed8ad52d9a
jberthiaume1138 <joenyc23@gmail.com>
2017-04-13 03:10:22 -0400

    centering styling

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-import style from './style/Nav.scss';
+import styles from './style/Nav.scss';
 
 export default class Nav extends React.Component {
 	render() {
 		return (
 			<nav>
-				<Link to="about" activeClassName={style.activeStyle}>About</Link>
-				<Link to="services" activeClassName={style.activeStyle} >Services</Link>
-				<Link to="contact" activeClassName={style.activeStyle}>Contact</Link>
+				<div className={styles.navWrapper}>
+					<Link to="about" activeClassName={styles.activeStyle}>About</Link>
+					<Link to="services" activeClassName={styles.activeStyle} >Services</Link>
+					<Link to="contact" activeClassName={styles.activeStyle}>Contact</Link>
+				</div>
 			</nav>
 		);
 	}
