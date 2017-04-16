59d557211c8671694e51ca9a0b0b4c7a4e54b994
jberthiaume1138 <joenyc23@gmail.com>
2017-04-15 22:50:13 -0400

    social media icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,6 +5,7 @@ import styles from './style/Footer.scss';
 
 import twitter from '../../images/twitter.svg';
 import linkedin from '../../images/linkedin.png';
+import email from '../../images/email-icon.svg';
 
 export default class Footer extends React.Component {
 	render () {
@@ -14,11 +15,12 @@ export default class Footer extends React.Component {
 					<div className={styles.footerNav}>
 						<Link to="about" activeClassName={styles.activeStyle}>About</Link>
 						<Link to="services" activeClassName={styles.activeStyle} >Services</Link>
-						<Link to="contact" activeClassName={styles.activeStyle}>Contact</Link>
+						{/* <Link to="contact" activeClassName={styles.activeStyle}>Contact</Link> */}
 					</div>
 					<div className={styles.social}>
-						<a href="https://twitter.com/carlfriedberg" target="_blank"><img src={twitter} height='35' /></a>
-						<a href="https://www.linkedin.com/in/carlfriedberg/" target="_blank"><img src={linkedin} height='25' /></a>
+						<a href="https://twitter.com/carlfriedberg" target="_blank"><img src={twitter} height='30' /></a>
+						<a href="mailto:carl@comets.com"><img src={email} height='30' /></a>
+						<a href="https://www.linkedin.com/in/carlfriedberg/" target="_blank"><img src={linkedin} height='30' /></a>
 					</div>
 					<div className={styles.footerText}>Downtown New York since 1991</div>
 				</div>
